1a90dbce4d5f1a8734f3589766059e99d6dd88e9
upup-ubuntu <2213311288@qq.com>
2021-01-24 19:39:51 +0800

    update to point when declare type method

--- lang-base/Beheave.go
+++ lang-base/Beheave.go
@@ -5,10 +5,10 @@ import "fmt"
 type Dog struct {
 }
 
-func (dog Dog) Eat() {
+func (dog *Dog) Eat() {
 	fmt.Println("dog eat")
 }
-func (dog Dog) Sleep() {
+func (dog *Dog) Sleep() {
 	fmt.Println("dog sleep")
 
 }
@@ -16,10 +16,10 @@ func (dog Dog) Sleep() {
 type Cat struct {
 }
 
-func (cat Cat) Eat() {
+func (cat *Cat) Eat() {
 	fmt.Println("cat eat")
 
 }
-func (cat Cat) Sleep() {
+func (cat *Cat) Sleep() {
 	fmt.Println("cat sleep")
 }
